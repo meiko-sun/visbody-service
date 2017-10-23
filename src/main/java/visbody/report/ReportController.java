058f9abed7ead09850884773bc59751b11599ccc
龙显林 <116166926@qq.com>
2017-10-23 17:21:03 +0800

    Report体测显示页面

---
+++ src/main/java/visbody/report/ReportController.java
@@ -0,0 +1,17 @@
+package visbody.report;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ReportController {
+
+    @RequestMapping("/report")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "report";
+    }
+}
