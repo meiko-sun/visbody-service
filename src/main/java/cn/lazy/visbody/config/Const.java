bc1e525f7b1de36b4d39aaa90ad4a2c7fc2f08bd
龙显林 <116166926@qq.com>
2017-10-27 17:44:04 +0800

    常量

---
+++ src/main/java/cn/lazy/visbody/config/Const.java
@@ -0,0 +1,10 @@
+package cn.lazy.visbody.config;
+
+/**
+ *
+ */
+public class Const {
+
+    public static final String SUANI_ID = "t30344wqtdja7jdr";
+    public static final String SUANI_SECRET = "SznNf5mSlveN4NmSD9YtoEUSFnf3XotR";
+}
